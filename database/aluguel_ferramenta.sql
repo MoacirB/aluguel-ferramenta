17dc39ee3503dea7e7102656a11b08aa67989ac1
MoacirB <moacirbatistatavares@gmail.com>
2020-04-04 23:55:07 -0300

    Update aluguel_ferramenta.sql

--- database/aluguel_ferramenta.sql
+++ database/aluguel_ferramenta.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 04-Abr-2020 às 18:05
+-- Generation Time: 05-Abr-2020 às 02:54
 -- Versão do servidor: 5.7.23
 -- versão do PHP: 7.2.10
 
@@ -49,15 +49,9 @@ CREATE TABLE IF NOT EXISTS `ferramenta` (
   `descricao` varchar(50) NOT NULL,
   `id_usuario` int(11) NOT NULL,
   `valor_dia` decimal(5,2) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `ferramenta`
---
-
-INSERT INTO `ferramenta` (`id`, `descricao`, `id_usuario`, `valor_dia`) VALUES
-(7, 'Mjolnir', 14, '150.50');
+  PRIMARY KEY (`id`),
+  KEY `fk_usuario` (`id_usuario`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -71,18 +65,14 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `usuario` varchar(30) NOT NULL,
   `senha` varchar(12) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
 INSERT INTO `usuario` (`id`, `usuario`, `senha`) VALUES
-(17, 'Marcelo', 'senha123'),
-(16, 'Josias', 'eoq'),
-(14, 'Moacir', 'npysnn'),
-(15, 'Lúcio', 'lva'),
-(11, 'Mario', 'alo123');
+(1, 'José', '1234');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
